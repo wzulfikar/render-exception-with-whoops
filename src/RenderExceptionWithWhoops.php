f6508a8e0d52b2b1a25d55fac79a9e9418750dfa
wzulfikar-MBA <wzulfikar@ymail.com>
2017-03-28 21:10:17 +0800

    initial commit

---
+++ src/RenderExceptionWithWhoops.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wzulfikar\WhoopsTrait;
+
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Util\Misc;
+
+trait RenderExceptionWithWhoops
+{
+    private function renderExceptionWithWhoops(\Exception $e, $editor = null)
+    {
+        $isAjax = Misc::isAjaxRequest();
+        $handler = $isAjax ? new JsonResponseHandler : new PrettyPageHandler;
+        if (!$isAjax && $editor) {
+            // https://github.com/filp/whoops/blob/master/docs/Open%20Files%20In%20An%20Editor.md
+            $handler->setEditor($editor);
+        }
+        
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler($handler);
+        return new \Illuminate\Http\Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
+    }
+}
