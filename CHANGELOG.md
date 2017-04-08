4ca679411715801c683af1316336b5fb4dc19717
wzulfikar_mba <me@wzulfikar.com>
2017-04-08 20:46:47 +0800

    Use PlainTextHandler in cli env

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+
+0.0.3 / 2017-04-08
+==================
+- **Added**
+    - Added new method `handleExceptionWithWhoops`
+- **Changed**
+    - Using `Whoops\PlainTextHandler` when error happens in cli environment
+    - use symfony response when available
+    - `renderExceptionWithWhoops`: 
+        - No more use of `Illuminate\Http\Response` as return value
+        - Now returns `Symfony\Component\HttpFoundation\Response` (only if class exists)
+
+0.0.2 / 2017-04-08
+==================
+- Changed PHP version to "^5.5.9 || ^7.0"
+
+0.0.1 / 2017-04-08
+==================
+- first functional version
+
--- src/RenderExceptionWithWhoops.php
+++ src/RenderExceptionWithWhoops.php
@@ -3,12 +3,13 @@
 namespace Wzulfikar\WhoopsTrait;
 
 use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PlainTextHandler;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Util\Misc;
 
 trait RenderExceptionWithWhoops
 {
-    private function renderExceptionWithWhoops(\Exception $e, $editor = null)
+    private function handleExceptionWithWhoops(\Exception $e, $editor = null)
     {
         $isAjax = Misc::isAjaxRequest();
         $handler = $isAjax ? new JsonResponseHandler : new PrettyPageHandler;
@@ -16,13 +17,30 @@ trait RenderExceptionWithWhoops
             // https://github.com/filp/whoops/blob/master/docs/Open%20Files%20In%20An%20Editor.md
             $handler->setEditor($editor);
         }
-        
+        if (Misc::isCommandLine()) {
+            $handler = new PlainTextHandler;
+
+            // disable trace in command line & only display error message
+            $handler->addTraceToOutput(false);
+        }
+
         $whoops = new \Whoops\Run;
         $whoops->pushHandler($handler);
-        return new \Illuminate\Http\Response(
-            $whoops->handleException($e),
-            $e->getStatusCode(),
-            $e->getHeaders()
-        );
+        return $whoops->handleException($e);
+    }
+
+    private function renderExceptionWithWhoops(\Exception $e, $editor = null)
+    {
+        $handledException = $this->handleExceptionWithWhoops($e, $editor);
+        
+        $responseClass = 'Symfony\Component\HttpFoundation\Response';
+        if (!class_exists($responseClass)) {
+            return new $responseClass(
+                $handledException,
+                $e->getStatusCode(),
+                $e->getHeaders()
+            );
+        }
+        return $handledException;
     }
 }
